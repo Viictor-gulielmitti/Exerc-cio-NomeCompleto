8bc982ed5897080d1842659dc2f49b15d61210ac
Viictor-gulielmitti <79719548+Viictor-gulielmitti@users.noreply.github.com>
2021-03-14 18:17:13 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Nome_Sobrenome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string primeiroNome;
+            string segundoNome;
+
+            Console.WriteLine("Digite seu primeiro nome:");
+            primeiroNome = Console.ReadLine();
+            Console.WriteLine("Digite seu sobrenome:");
+            segundoNome = Console.ReadLine();
+            Console.WriteLine($"Nome completo: {primeiroNome} {segundoNome}");
+            Console.Write($"Nome de catalogo:{segundoNome}".ToUpper());
+            Console.Write($", {primeiroNome}");
+
+        }
+    }
+}
